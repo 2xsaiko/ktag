d96a88338316921de26e23e3e805575816dc99c2
therealfarfetchd <git@marco-rebhan.de>
2018-05-13 12:25:46 +0200

    add publish script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "ktag"
+group = "therealfarfetchd.ktag"
 version = "1.0.0"
 
 buildscript {
   var kotlin_version: String by extra
-  kotlin_version = "1.2.10"
+  kotlin_version = "1.2.+"
 
   repositories {
     mavenCentral()
@@ -14,12 +14,10 @@ buildscript {
   dependencies {
     classpath(kotlinModule("gradle-plugin", kotlin_version))
   }
-
 }
 
-apply {
-  plugin("kotlin")
-}
+apply { from("publish.gradle.kts") }
+apply { plugin("kotlin") }
 
 val kotlin_version: String by extra
 
